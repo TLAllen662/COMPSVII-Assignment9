e63f705498549ad1cdd6f5881ff43553d77a6bfd
TLAllen662 <tlallen1419@gmail.com>
2025-11-02 17:28:01 -0600

    Add endpoint function to fetch and log GIFs from Giphy API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("script.js loaded");
+endpoint();
+function endpoint() {
+    console.log("Endpoint function executed");
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=Wr4cT04axZ3ByRd0m1pHDdO0KcYxfCE1&q=Test&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips')
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data fetched from Giphy API:", data);
+            const gifs = data.data;
+            gifs.forEach(gif => {
+                console.log("GIF URL:", gif.images.fixed_height.url);
+            })
+        })
+}
